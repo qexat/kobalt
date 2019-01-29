a4d3cde0f03f890842a458ba572d6bbd06b192b9
qexat <43090614+qexat@users.noreply.github.com>
2019-01-29 20:05:14 +0100

    Add files via upload
    To see the file, you need to set "background" file in a folder called "content".

---
+++ script.js
@@ -0,0 +1,8 @@
+function displayWindow() {
+    document.getElementsByClassName("window")[0].style.top = "0px";
+    document.getElementsByClassName("window")[0].style.bottom = "0px";
+}
+function hideWindow() {
+    document.getElementsByClassName("window")[0].style.top = "100%";
+    document.getElementsByClassName("window")[0].style.bottom = "100%";
+}
